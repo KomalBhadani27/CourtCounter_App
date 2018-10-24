f82dbe7db113f3b3ff5adbe9e350c382d25cf555
Shivansh <iit2017097@iiita.ac.in>
2018-10-24 22:40:54 +0530

    Added Winner

--- app/src/main/java/com/example/android/courtcounter/Main2Activity.java
+++ app/src/main/java/com/example/android/courtcounter/Main2Activity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -20,6 +21,7 @@ public class Main2Activity extends AppCompatActivity {
 
     ListView listView;
     ListView listView1;
+    int total1=0,total2=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,25 @@ public class Main2Activity extends AppCompatActivity {
              array = bundle.getStringArrayList(MainActivity.EXTRA_MESSAGE);
              array1= bundle.getStringArrayList(MainActivity.Key);
         }
+        TextView winner = findViewById(R.id.winner_text);
+        assert array != null;
+        assert array1 != null;
+        if(!array.isEmpty() && !array1.isEmpty()) {
+            for(int i=0;i<array.size();i++) {
+                total1+=Integer.parseInt(array.get(i));
+            }
+            for(int i=0;i<array1.size();i++) {
+                total2+=Integer.parseInt(array1.get(i));
+            }
+        }
+        if(total1>total2) {
+            winner.setText("Team 1");
+        }
+        else if(total2>total1) {
+            winner.setText("Team 2");
+        } else {
+            winner.setText("Tie");
+        }
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
                 this,
@@ -50,5 +71,5 @@ public class Main2Activity extends AppCompatActivity {
 
         listView1.setAdapter(arrayAdapter1);
     }
-    }
+}
 
